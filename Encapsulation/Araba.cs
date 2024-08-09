ba9b1710afb03c398e8cd48b5fc886fd955ad6e7
Hasan İncel <h_incel@hotmail.com>
2024-08-10 01:55:20 +0300

    Add project files.

---
+++ Encapsulation/Araba.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Araba
+    {
+        private string _marka;
+        private string _model;
+        private string _renk;
+        private int _kapisayisi;
+
+        // Sınıfın constructerı
+        public Araba(string marka, string model, string renk, int kapisayisi)
+        {
+            _marka = marka;
+            _model = model;
+            _renk = renk;
+            Kapisayisi = kapisayisi;
+        }
+
+        // Kapı sayısı özelliği
+        public int Kapisayisi
+        {
+            get { return _kapisayisi; }
+            set
+            {
+                if (value == 2 || value == 4)
+                    _kapisayisi = value;
+                else
+                {
+                    Console.WriteLine("Geçersiz kapı sayısı. Kapı sayısı -1 olarak ayarlandı.");
+                    _kapisayisi = -1;
+                }
+            }
+        }
+
+        // Sınıfın bilgilerini yazdırmak için bir yöntem
+        public void Bilgiler()
+        {
+            Console.WriteLine("\nArabanın Markası: " + _marka);
+            Console.WriteLine("Arabanın Modeli: " + _model);
+            Console.WriteLine("Arabanın Rengi: " + _renk);
+            Console.WriteLine("Arabanın Kapı Sayısı: " + _kapisayisi);
+        }
+    }
+}
+
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Encapsulation;
+
+// Geçerli kapı sayısı ile araba oluşturulması
+Araba araba1 = new Araba("Renault", "Megane", "Siyah", 4);
+
+// Geçersiz kapı sayısı ile araba oluşturulması
+Araba araba2 = new Araba("Volkswagen", "Polo" , "Beyaz", 3);
+
+araba1.Bilgiler();
+araba2.Bilgiler();
